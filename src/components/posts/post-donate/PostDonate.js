a6e220cb43c45ed09603e0fba4448d035672f47b
angrycode2018 <47102718+angrycode2018@users.noreply.github.com>
2021-01-29 01:26:47 +0700

    Implement PaymentContext & post payment to server

--- src/components/posts/post-donate/PostDonate.js
+++ src/components/posts/post-donate/PostDonate.js
@@ -1,7 +1,10 @@
 import React, { useState, useContext, useEffect } from 'react'
-import PaymentContext from '../../../PaymentContext';
+import PaymentContext from '../../../PaymentContext'
+import JsonServer from '../../../services/JsonServer'
 
-export default function PostDonate() {
+const api = new JsonServer()
+
+export default function PostDonate({charitiesId, name, currency}) {
 
     const [checkbox, setCheckbox] = useState({});
     const [payments, setPayments] = useState([]);
@@ -14,37 +17,47 @@ export default function PostDonate() {
     const handleChange = ({target}) => {
         checkbox.name !== 'undefined' && checkbox.name !== target.name ? checkbox.checked = false: checkbox.checked = target.checked;
         setCheckbox(target);
-        const {type, name ,value, checked} = target
-        console.log(`Type: ${type}, Name: ${name}, Value: ${value}, Checked: ${checked}`)
-        console.log('Target', target)
+        // const {type, name ,value, checked} = target
+        // console.log(`Type: ${type}, Name: ${name}, Value: ${value}, Checked: ${checked}`)
     }
 
     const handleSubmit = (event) => {
         event.preventDefault()
-       // console.log('Submit: ', event.target)
-       console.log('Hahaha:',payments)
+        const newPayment = { 
+            charitiesId: parseInt(charitiesId, 10), 
+            amount: parseInt(checkbox.value, 10) , 
+            currency: currency, 
+            id: ++payments.length 
+        }
+        //const newPayment = {name: 'Somchai', old: 44, height: 222} 
+        api.post(newPayment, 'payments').then(res => res === true ? 
+            PaymentPromise.then(data => {
+                setPayments(data); 
+                alert('Thank you for donation.'); 
+            }).catch(e => console.error('Something wrong!, ', e)) : false)
+        .catch(error => console.error('Post fail ', error))
     }
 
 
     return (
         <div className="Form__donate">
-            <header>Select the amout to donate in THB</header>
+            <header>Select the amout to donate in {currency}</header>
             <form onSubmit={handleSubmit}>
                 <div className="Form__checkbox">
                     <label>
-                    <input name="box1" type="checkbox" value="10" onChange={handleChange} />10
+                        <input name="box1" type="checkbox" value="10" onChange={handleChange} />10
                     </label>
                     <label>
-                    <input name="box2" type="checkbox" value="20" onChange={handleChange} />20
+                        <input name="box2" type="checkbox" value="20" onChange={handleChange} />20
                     </label>
                     <label>
-                    <input name="box3" type="checkbox" value="50" onChange={handleChange} />50
+                        <input name="box3" type="checkbox" value="50" onChange={handleChange} />50
                     </label>
                     <label>
-                    <input name="box4" type="checkbox" value="100" onChange={handleChange} />100
+                        <input name="box4" type="checkbox" value="100" onChange={handleChange} />100
                     </label>
                     <label>
-                    <input name="box5" type="checkbox" value="500" onChange={handleChange} />500
+                        <input name="box5" type="checkbox" value="500" onChange={handleChange} />500
                     </label>
                 </div>
                 <input type="submit" value="Pay" style={{fontSize: '25px', padding: '5px', width: '160px', cursor: 'pointer'}} />
