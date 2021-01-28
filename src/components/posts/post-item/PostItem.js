ade8278804f4dd345b1bf698304de76c3a844c2a
angrycode2018 <47102718+angrycode2018@users.noreply.github.com>
2021-01-28 14:18:42 +0700

    Move file to Components folder

---
+++ src/components/posts/post-item/PostItem.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+import Button from '../../button/Button'
+
+export default function PostItem(props) {
+    const {id, name, image, currency} = props
+    const [hidden, setHidden] = useState(true)
+
+    return ( 
+        <div className="Post__item">
+            <header className="Post__title">
+                {name}
+                <Button onClick={()=>setHidden(false)}/>
+            </header>
+            <div className={`Post__donate ${hidden ? 'hide' : ''}`} onMouseLeave={()=>setHidden(true)}>
+                <button type="button" className="Btn__close" onClick={()=>setHidden(true)}>X</button>
+                <div className="Post__donate__content">
+                Nunc aliquet leo et magna luctus maximus. Ut malesuada sollicitudin placerat. Donec tincidunt sapien at mollis sagittis. Nam efficitur dui faucibus accumsan consectetur. Aliquam tempor eleifend sagittis. Vestibulum quis ex ligula. Sed lacinia magna lacus, at interdum magna aliquam eget.
+                </div>
+            </div>
+            <img className="Post__image" src={`./images/${image}`}/>
+        </div>
+    )
+}
+
+// src={`https://source.unsplash.com/random?sig=${id}`}
+//<button className="Button__donate">Donate</button>
