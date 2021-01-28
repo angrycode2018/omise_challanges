dcdd62f054cbb6d35bd478c5c92329264047c4c0
angrycode2018 <47102718+angrycode2018@users.noreply.github.com>
2021-01-28 10:37:22 +0700

    Update PostItem Component

--- src/posts/post-item/PostItem.js
+++ src/posts/post-item/PostItem.js
@@ -1,16 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Button from '../../button/Button'
 
 export default function PostItem(props) {
     const {id, name, image, currency} = props
-    const title = 'Post Item Component'
-
+    const [hidden, setHidden] = useState(true)
 
     return ( 
         <div className="Post__item">
-            <header className="Post__title">{name}</header>
+            <header className="Post__title">
+                {name}
+                <Button onClick={()=>setHidden(false)}/>
+            </header>
+            <div className={`Post__donate ${hidden ? 'hide' : ''}`}>
+                This content is hidden.
+                <button type="button" className="Btn__close" onClick={()=>setHidden(true)}>X</button>
+            </div>
             <img className="Post__image" src={`./images/${image}`}/>
         </div>
     )
 }
 
 // src={`https://source.unsplash.com/random?sig=${id}`}
+//<button className="Button__donate">Donate</button>
